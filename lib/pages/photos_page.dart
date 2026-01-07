9163945ba4f0cd01a6fcc5d23892f2149e4544d1
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2026-01-07 18:04:49 +0500

    imp 2

--- lib/pages/photos_page.dart
+++ lib/pages/photos_page.dart
@@ -1,5 +1,6 @@
 // https://jsonplaceholder.typicode.com/photos
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class PhotosPage extends StatefulWidget {
@@ -10,6 +11,27 @@ class PhotosPage extends StatefulWidget {
 }
 
 class _PhotosPageState extends State<PhotosPage> {
+  bool isLoading = false;
+  final dio = Dio();
+  List data = [];
+
+  void getPhotos() async {
+    isLoading = true;
+    setState(() {});
+
+    final response = await dio.get("https://jsonplaceholder.typicode.com/photos",);
+    data = response.data;
+
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getPhotos();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
