6799d8b0bb4bb9f39524e87d92b6d54a3a026556
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2026-01-07 18:17:20 +0500

    imp 3

--- lib/pages/photos_page.dart
+++ lib/pages/photos_page.dart
@@ -1,5 +1,7 @@
 // https://jsonplaceholder.typicode.com/photos
 
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -34,6 +36,23 @@ class _PhotosPageState extends State<PhotosPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: Text(data[index]['id'].toString()),
+                title: Text(data[index]['title']),
+                subtitle: Text(data[index]['url']),
+                trailing: Text(data[index]['albumId'].toString()),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
