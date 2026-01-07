d3fbe1ce1e416fd2e110ab7ebcef70cdf87c9ce4
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2026-01-07 17:26:18 +0500

    imp

---
+++ lib/pages/comments_page.dart
@@ -0,0 +1,17 @@
+// https://jsonplaceholder.typicode.com/comments
+
+import 'package:flutter/material.dart';
+
+class CommentsPage extends StatefulWidget {
+  const CommentsPage({super.key});
+
+  @override
+  State<CommentsPage> createState() => _CommentsPageState();
+}
+
+class _CommentsPageState extends State<CommentsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
